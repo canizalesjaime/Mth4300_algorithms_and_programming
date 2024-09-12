b3fde06a6edb90121c5da83e75f0fe5b13bfc9b2
canizalesjaime <canizales.jaime16@gmail.com>
2024-09-12 15:20:35 -0400

    added hw template

---
+++ class_notes/week3/loops_and_arrays-09_12/hw_template.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std; 
+
+void question1();
+void question2();
+void question3();
+
+
+int main()
+{
+    question1();
+    cout<<endl<<endl;
+
+    question2();
+    cout<<endl<<endl;
+
+    question3();
+    cout<<endl<<endl;
+    return 0;
+}
+
+void question1()
+{
+
+}
+
+void question2()
+{
+    
+}
+
+void question3()
+{
+    
+}

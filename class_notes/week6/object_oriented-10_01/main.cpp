02f1900b728444e03dba8f472354a525a3b0cb70
canizalesjaime <canizales.jaime16@gmail.com>
2024-10-02 11:41:36 -0400

    in class updates

--- class_notes/week6/object_oriented-10_01/main.cpp
+++ class_notes/week6/object_oriented-10_01/main.cpp
@@ -1,10 +1,62 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-   
+// Define a class
+class Car {
+public:
+    string brand;
+    string model;
+    int year;
+
+    // Constructor to initialize the object
+    Car(string b, string m, int y) {
+        brand = b;
+        model = m;
+        year = y;
+    }
+
+    // Member function to display car information
+    void displayInfo() {
+        cout << "Brand: " << brand << ", Model: " << model << ", Year: " << year << endl;
+    }
+};
+
+
+class BankAccount {
+private:
+    double balance;
+
+public:
+    // Constructor to initialize balance
+    BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    // Public method to deposit money
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    // Public method to retrieve the balance
+    double getBalance() {
+        return balance;
+    }
+};
+
+int main() {
+    // // Create an object of the Car class
+    // Car car1("Toyota", "Camry", 2020);
+    
+    // // Call the member function
+    // car1.displayInfo();  // Output: Brand: Toyota, Model: Camry, Year: 2020
+
+    // Car car2("Nissan", "Altima", 2018);
+    // car2.displayInfo();
+
+    BankAccount account(1000);  // Create an object with initial balance of 1000
+
+    account.deposit(500);       // Deposit 500
+    cout << "Current balance: " << account.getBalance() << endl;  // Output: Current balance: 1500
 
     return 0;
 }
--- hw_solutions/hw2/hw2_template.cpp
+++ hw_solutions/hw2/hw2_template.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-int recursive_sum(int arr[]);
+int recursive_sum(int arr[], int size_of_arr);
+bool check_rotation(int arr1[],int arr2[],int size_of_arr1,int size_of_arr2);
 
 int main()
 {
@@ -17,29 +18,29 @@ int main()
     // Question 2
     // only an example, I should be able to change arrays and function still works
     int arr1[5]={1,2,3,4,5}; 
-    int sum1=recursive_sum(arr1);
+    int sum1=recursive_sum(arr1, 5);
     cout<<"sum: "<<sum1<<endl;
 
     int arr2[5]={10,12,35,41,-51}; 
-    int sum2=recursive_sum(arr2);
+    int sum2=recursive_sum(arr2,5);
     cout<<"sum: "<<sum2<<endl;
     
     // Question 3
     int arr3[4]={10,20,30,40};
     int arr4[4]={30,40,10,20};
-    cout<<check_rotation(arr3,arr4)<<endl;
+    cout<<check_rotation(arr3,arr4,4,4)<<endl;
 
     return 0;
 }
 
 
-int recursive_sum(int arr[])
+int recursive_sum(int arr[], int size_of_arr1)
 {
     //FILL IN CODE HERE
 }
 
 
-bool check_rotation(int arr[],int arr[])
+bool check_rotation(int arr1[],int arr2[],int size_of_arr1,int size_of_arr2)
 {
     //FILL IN CODE HERE
 }
